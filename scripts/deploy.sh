57f52df2725e3118a979bdf66ceecb25200e8baf
Ben <ben@bensites.com>
2016-07-07 22:26:35 -0500

    changed how gh-deploy works

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-if [[ ! ( "$TRAVIS_BRANCH" == "master" || "$(git symbolic-ref --short HEAD)" == "master" ) ]]
+if [[ -z $(git status -s) ]]
 then
-  echo "You can only deploy from master." >&2
-  exit 0
-fi
+    if [[ "$TRAVIS_BRANCH" != "master" ]]
+    then
+      echo "You can only deploy from master." >&2
+      exit 0
+    fi
 
-git add -A .
-git -c user.name='Whose News Auto Deploy' -c user.email=whosenews@bensites.com commit -m "GitHub Pages deploy @$(git rev-parse --short HEAD)"
-git push origin master
-git subtree push --prefix build/homepage origin gh-pages
+    $commit = `git rev-parse --short HEAD`
+
+    cd build/homepage
+    git init
+    git add .
+    git -c user.name='Whose News Auto Deploy' -c user.email=whosenews@bensites.com commit -m "GitHub Pages deploy @$commit"
+    git push --force --quiet "https://${GH_TOKEN}@github.com/penne12/WhoseNews.git" master:gh-pages
+else
+    echo "No changes, skipping deploy." >&2
+    exit 0
+fi
--- scripts/travisci-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-npm run gh-deploy
