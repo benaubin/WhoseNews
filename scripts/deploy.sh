8aefee3c8eb0bee8d8967953b1861e3dc1f2fec9
Ben <ben@bensites.com>
2016-07-07 21:32:07 -0500

    fixed script to only deploy from master

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" != "master" ] || [ "$(git symbolic-ref --short HEAD)" != "master" ]
+if [[ ! ( "$TRAVIS_BRANCH" == "master" || "$(git symbolic-ref --short HEAD)" == "master" ) ]]
 then
   echo "You can only deploy from master." >&2
   exit 0
 fi
-
-
 
 git add -A .
 git -c user.name='Whose News Auto Deploy' -c user.email=whosenews@bensites.com commit -m "GitHub Pages deploy @$(git rev-parse --short HEAD)"
