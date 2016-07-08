aa544c0edefd247bf876ee2265f59306675f4bcf
Ben <ben@bensites.com>
2016-07-07 23:05:21 -0500

    fixed deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-if [[ -z $(git status -s) ]]
+
+if [[ "$TRAVIS_BRANCH" != "master" ]]
 then
-    if [[ "$TRAVIS_BRANCH" != "master" ]]
-    then
-      echo "You can only deploy from master." >&2
-      exit 0
-    fi
-
-    $commit = `git rev-parse --short HEAD`
-
-    cd build/homepage
-    git init
-    git add .
-    git -c user.name='Whose News Auto Deploy' -c user.email=whosenews@bensites.com commit -m "GitHub Pages deploy @$commit"
-    git push --force --quiet "https://${GH_TOKEN}@github.com/penne12/WhoseNews.git" master:gh-pages
-else
-    echo "No changes, skipping deploy." >&2
-    exit 0
+  echo "You can only deploy from master." >&2
+  exit 0
 fi
+
+$commit = `git rev-parse --short HEAD`
+
+cd build/homepage
+git init
+git add .
+git -c user.name='Whose News Auto Deploy' -c user.email=whosenews@bensites.com commit -m "GitHub Pages deploy @$commit"
+git push --force --quiet "https://${GH_TOKEN}@github.com/penne12/WhoseNews.git" master:gh-pages
